3e0d27d27b98fe667c0ef7d1cb86e32104be68ce
josegustavo12 <josegustavovictor@gmail.com>
2024-11-06 18:20:55 -0300

    aasasjsajsk

--- Algoritmo-e-Estrutura-de-Dados-1/Avaliação Prática/quest7/main.c
+++ Algoritmo-e-Estrutura-de-Dados-1/Avaliação Prática/quest7/main.c
@@ -8,25 +8,30 @@ int main()
     Tree* tree = createTree();
 
     printf("\n\nInsert 17");
-    tree->root = insert(tree->root, 17);
+    tree->root = insert(tree->root, 61);
 
     printf("\n\nInsert 6");
-    tree->root = insert(tree->root, 6);
+    tree->root = insert(tree->root, 34);
 
     printf("\n\nInsert 35");
-    tree->root = insert(tree->root, 35);
+    tree->root = insert(tree->root, 19);
 
     printf("\n\nInsert 4");
-    tree->root = insert(tree->root, 4);
+    tree->root = insert(tree->root, 20);
 
     printf("\n\nInsert 14");
-    tree->root = insert(tree->root, 14);
+    tree->root = insert(tree->root, 50);
 
     printf("\n\nInsert 23");
-    tree->root = insert(tree->root, 23);
+    tree->root = insert(tree->root, 55);
 
     printf("\n\nInsert 48");
-    tree->root = insert(tree->root, 48);
+    tree->root = insert(tree->root, 67);
+    tree->root = insert(tree->root, 66);
+    tree->root = insert(tree->root, 64);
+    tree->root = insert(tree->root, 94);
+    tree->root = insert(tree->root, 70);
+    tree->root = insert(tree->root, 95);
 
     printf("\n\nBuscar 14\n");
     int achou = search(tree->root, 14);
