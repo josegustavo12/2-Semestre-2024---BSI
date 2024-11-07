a676234a29f16ce598f12c14ecd9f3c29e24f32c
mouramanasses <manassesmoura10@gmail.com>
2024-11-07 18:45:23 -0300

    questao 7 comentada

--- Algoritmo-e-Estrutura-de-Dados-1/Avaliação Prática/quest7/main.c
+++ Algoritmo-e-Estrutura-de-Dados-1/Avaliação Prática/quest7/main.c
@@ -7,30 +7,41 @@ int main()
 
     Tree* tree = createTree();
 
-    printf("\n\nInsert 17");
+//fazendo o teste com a árvore presente no enunciado da questão
+    printf("\n\nInsert 61");
     tree->root = insert(tree->root, 61);
 
-    printf("\n\nInsert 6");
+    printf("\n\nInsert 34");
     tree->root = insert(tree->root, 34);
 
-    printf("\n\nInsert 35");
+    printf("\n\nInsert 19");
     tree->root = insert(tree->root, 19);
 
-    printf("\n\nInsert 4");
+    printf("\n\nInsert 20");
     tree->root = insert(tree->root, 20);
 
-    printf("\n\nInsert 14");
+    printf("\n\nInsert 50");
     tree->root = insert(tree->root, 50);
 
-    printf("\n\nInsert 23");
+    printf("\n\nInsert 55");
     tree->root = insert(tree->root, 55);
 
-    printf("\n\nInsert 48");
+    printf("\n\nInsert 67");
     tree->root = insert(tree->root, 67);
+
+    printf("\n\nInsert 66");
     tree->root = insert(tree->root, 66);
+
+    printf("\n\nInsert 64");
     tree->root = insert(tree->root, 64);
+
+    printf("\n\nInsert 94");
     tree->root = insert(tree->root, 94);
+
+    printf("\n\nInsert 70");
     tree->root = insert(tree->root, 70);
+
+    printf("\n\nInsert 95");
     tree->root = insert(tree->root, 95);
 
     printf("\n\nBuscar 14\n");
@@ -55,17 +66,17 @@ int main()
     node = getMinNode(tree->root);
     printf("\n\nMenor: %d", node->data);
 
-    printf("\n\nPercurso pr�-ordem\n");
+    printf("\n\nPercurso pre-ordem\n");
     strPreorder(tree->root);
 
     printf("\n\nPercurso em-ordem\n");
     strInorder(tree->root);
 
-    printf("\n\nPercurso p�s-ordem\n");
+    printf("\n\nPercurso pos-ordem\n");
     strPostorder(tree->root);
 
 
-    printf ("\n\nSoma dos caminhos\n:");
+    printf ("\n\nSomas dos caminhos:");
     caminhos(tree);
 
 }
